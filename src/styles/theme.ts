968a19e703170b86a8cf63b133c12048f74c3dcc
Stelian Fedorca <stelian.fedorca25@gmail.com>
2025-11-29 13:30:38 +0200

    Create rem based breakpoints using pixel values

---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+/*
+    This need to be inserted as props to the ThemeProvider from styled-components
+    !! Currently not used as I am using CSS variables !!
+*/
+export const theme = {
+  colors: {
+    primary: "blue",
+  },
+};
