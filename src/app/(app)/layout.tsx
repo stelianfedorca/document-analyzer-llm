29a22a6e1896f5aecbe686e85f4a16a32f15d4bc
Stelian Fedorca <stelian.fedorca25@gmail.com>
2025-12-17 11:11:23 +0200

    Update font family to Inter

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "@/app/globals.css";
 import { AppShell } from "@/components/layouts/AppShell";
 import Providers from "../providers";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -26,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${inter.variable}`}>
         <Providers>
           <AppShell>{children}</AppShell>
         </Providers>
